4003c4c1230aed163876b181dbdbdc27b4f51ca2
MIRZA OMAR <58821151+omarbaig007212@users.noreply.github.com>
2021-06-21 12:48:49 +0530

    Create Scrambled_String_Recursive.java

---
+++ MATRIX/TYPES_OF_MCM/Scrambled_String_Recursive.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+public class Scrambled_String_Recursive {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		String sr=sc.next();
+		if(s.length()!=sr.length())
+			System.out.println("False");
+		else
+			System.out.println(solve(s,sr));
+	}
+	public static boolean solve(String s,String sr)
+	{
+		if(s.equals(sr))
+			return true;
+		if(s.length()<=1 || sr.length()<=1)
+			return false;
+		int n=s.length();
+		boolean flag=false;
+		for(int i=1;i<n;i++)
+		{
+			if((solve(s.substring(0,i), sr.substring(0,i))&&(solve(s.substring(n-i), sr.substring(n-i))))||
+			((solve(s.substring(0,i), sr.substring(i))&&(solve(s.substring(i),sr.substring(0,i)))))==true)
+			{
+				flag= true;
+				break;
+			}
+		}
+		return flag;
+	}
+}
