bb5d4bb73fea67878d79d1df0b446dc7b3b46cac
MIRZA OMAR <58821151+omarbaig007212@users.noreply.github.com>
2021-08-06 16:05:42 +0530

    Update level_order.java

--- code_exp/trees/level_order.java
+++ code_exp/trees/level_order.java
@@ -23,9 +23,9 @@
             }
         }
     }
-  }
-//Step-------------------------------11111
+}
 
+//Step ------------------------------------------ 2222222222222  (Make a flag(-1) to know that this is till this level and then print "\n" when encountered;)
 
 public static void levelOrderLinewise(Node node){
       Queue<Node> queue = new ArrayDeque<>();
@@ -48,5 +48,25 @@ public static void levelOrderLinewise(Node node){
       }
     }
 
+//Step-------------------------------11111 (Normal mainQ and currQ where the data is stored in both and exchanged;)
 
-//Step ------------------------------------------ 2222222222222
+ public static void levelOrderLinewise(Node node){
+    // write your code here
+    Queue<Node> mainQ=new ArrayDeque<>();
+    mainQ.add(node);
+    while(mainQ.size()>0)
+    {
+        int siz=mainQ.size();
+        for(int i=0;i<siz;i++)
+        {
+            node=mainQ.remove();
+            System.out.print(node.data+" ");
+            for(Node child:node.children)
+            {
+                mainQ.add(child);
+            }
+        }
+        System.out.println();
+    }
+  }
+//step---------------------------------------- 333333333333333 (Make a counter vairable to store the number on each level and then iterate and store until the full iteration);
