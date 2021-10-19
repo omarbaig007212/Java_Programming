6bcd13daab5fe846bdcd8178a7da947d9a353693
MIRZA OMAR <58821151+omarbaig007212@users.noreply.github.com>
2021-10-19 12:02:04 +0530

    Create pattern9.java

---
+++ Pattern/pattern9.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main{
+  public static void main(String[]args)
+  {
+    pattern(5);
+  }
+  public static void pattern(int n)
+  {
+    for(int i=1;i<=n;i++)
+    {
+      int j=1;
+      for(;j<i;j++)
+      {
+        System.out.print(" ");
+      }
+      int count=0;
+      for(;j<=n;j++)
+      {
+        System.out.print("*");
+        count++;
+      }
+      for(j=1;j<count;j++)
+      {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
