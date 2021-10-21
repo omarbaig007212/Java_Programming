696c25a4e92171b84c768bd69f7916570736808e
MIRZA OMAR <58821151+omarbaig007212@users.noreply.github.com>
2021-10-21 13:57:59 +0530

    Update pattern17.java

--- Pattern/pattern17.java
+++ Pattern/pattern17.java
@@ -28,3 +28,45 @@ public class Main {
     }
   }
 }
+
+
+//EASY!!!!!!!!!!!!!!!
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    pattern(4);
+  }
+
+  public static void pattern(int n) {
+    int count=n;
+    int val=0;
+    for(int i=1;i<2*n;i++)
+    {
+      if(i<=n)
+      {
+        count--;
+        val++;
+      }
+      else
+      {
+        count++;
+        val--;
+      }
+      for(int j=1;j<=count;j++)
+      {
+        System.out.print(" ");
+      }
+      for(int j=val;j>=1;j--)
+      {
+        System.out.print(j);
+      }
+      for(int j=2;j<=val;j++)
+      {
+        System.out.print(j);
+      }
+      System.out.println();
+    }
+  }
+}
