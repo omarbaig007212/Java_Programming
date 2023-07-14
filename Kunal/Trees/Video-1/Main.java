a16557fedcca4f3fc6d1ddfef6f970950e9b3577
MIRZA OMAR <58821151+omarbaig007212@users.noreply.github.com>
2023-07-14 21:25:14 +0530

    Create Main.java

---
+++ Kunal/Trees/Video-1/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+	public static void main(String[]args)
+	{
+		BinarySearchTree bt = new BinarySearchTree();
+		// int arr[]={5,2,7,1,4,6,9,8,3,10}; // True
+		// int arr[]={1,2,3,4,5,6,7,8,9,10}; // False
+		// bt.publicate(arr);
+		// bt.publicateSort(arr);
+		// bt.display();
+		// System.out.println(" Is the tree balanced : ? "+bt.balanced());
+	}
+}
